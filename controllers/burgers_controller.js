452546982c4c325f4440e0dfccaabe7b7957daaf
Amy Keneson <amy.l.keneson@icloud.com>
2020-10-02 12:55:21 -0500

    burgers_controller.js edited

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,12 +3,12 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model(burgers.js to use its database functions)
-var burgers = require("../models/burgers");
+var burger = require("../models/burgers");
 
 // create routes and set up logic within the routes where required.
 
 router.get("/", function(req, res){
-    burgers.all(function(data){
+    burger.all(function(data){
         var hbsObject = {
             burgers: data
         };
@@ -18,34 +18,35 @@ router.get("/", function(req, res){
 })
 
 router.post("/api/burgers", function(req, res){
-    burgers.create(["burger_name", "devour"], [req.body.name, req.body. devour], function (result) {
-        res.json({ id: result.insertId });
+    burger.create([
+        "burger_name"
+    ], [
+        req.body.name
+    ], function (data) {
+        res.redirect("/");
     });
 });
 
 router.put("/api/burgers/:id", function(req, res){
-    burgers.update({
-        devour: req.body.devour
-    }, 'id',
-    req.params.id,
-    function(result){
-        if(result.changedRows ==0){
-            // if no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        }
-        res.status(200).end();
+    var condition = "id = " + req.params.id;
+
+    burger.update({
+        devour: true
+    }, condition,
+    function(data){
+        res.redirect('/');
     })
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
-    burgers.delete(req.params.id, function(result){
-        if (result.affectedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.json(result);
-        }
-    });
-});
+// router.delete("/api/burgers/:id", function(req, res) {
+//     burgers.delete(req.params.id, function(result){
+//         if (result.affectedRows == 0) {
+//             return res.status(404).end();
+//         } else {
+//             res.json(result);
+//         }
+//     });
+// });
 
 //  export routes for server.js to use
 module.exports = router;
