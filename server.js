a4cf22e4e67c9e38179220385f6a551d047499f6
Amy Keneson <amy.l.keneson@icloud.com>
2020-10-02 13:53:59 -0500

    server.js edited

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
-var express = require("express");
+// Pull in required dependencies
+var express = require('express');
 
-var PORT = process.env.PORT || 3000;
+// body-parser extract the entire body portion of an incoming request stream and exposes it on req.body.
+var bodyParser = require('body-parser');
+
+// The methodOverride() middleware is for requests from clients that only natively support simple verbs like GET and POST. So in those cases you could specify a special query field (or a hidden form field for example) that indicates the real verb to use instead of what was originally sent. That way your backend .put()/.delete()/.patch()/etc. routes don't have to change and will still work and you can accept requests from all kinds of clients.
+var methodOverride = require('method-override');
+
+var port = process.env.PORT || 3000;
 
 var app = express();
 
-// serve static content for the app from the public director in the application directory
-app.use(express.static("public"));
+// Serve static content for the app from the 'public' directory
+app.use(express.static(process.cwd() + '/public'));
 
-// parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Set Handlebars
-var exphbs = require("express-handlebars");
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
-app.set("view engine", "handlebars");
+// Set Handlebars as the view engine
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them
-var routes = require("./controllers/burgers_controller");
+var routes = require('./controllers/burgers_controller.js');
 
-app.use(routes);
+app.use('/', routes);
 
-// start server that it can begin listening to client requests
-app.listen(PORT, function(){
-    console.log("App now listening at localhost: "+ PORT)
-    console.log("http://localhost:" + PORT);
-})
-
+app.listen(port);
+console.log ("Server listening on: http://localhost:" + port);
